babea70bb40dd23c1fe656b4ab735db11bf6c60d
Crowdbotics <team@crowdbotics.com>
2024-02-05 20:12:51 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "parallelapp-chrome-45978",
+  url: "https://parallelapp-chrome-45978.botics.co",
+  api: "https://parallelapp-chrome-45978.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
